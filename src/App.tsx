bb44bf648ca4994f6efd66ee5e1cac95018578ca
briantimothykarani <timothykaranibrian@gmail.com>
2026-02-04 14:50:44 +0300

    fix all

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import BrokersPage from "./pages/BrokersPage";
 import Landingpage from "./pages/Landingpage";
 import ComparePage from "./pages/ComparePage";
 import BrokerCard from "./components/BrokerCard";
 
-const App = () => {
-  return (
-    <HashRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Landingpage />} />
-          <Route path="/brokers" element={<BrokersPage />} />
-          <Route path="/brokers/comparepage" element={<ComparePage />} />
-          <Route path="/brokers/:id" element={<BrokerCard />} />
-        </Routes>
-      </Layout>
-    </HashRouter>
-  );
-};
+const App = () => (
+  <BrowserRouter basename="/broker-compare">
+    {" "}
+    {/* <-- here */}
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Landingpage />} />
+        <Route path="/brokers" element={<BrokersPage />} />
+        <Route path="/brokers/comparepage" element={<ComparePage />} />
+        <Route path="/brokers/:id" element={<BrokerCard />} />
+      </Routes>
+    </Layout>
+  </BrowserRouter>
+);
 
 export default App;
