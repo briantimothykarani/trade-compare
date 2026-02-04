f9be7e8f7b3aa6ef75ab19df70a08b38b1c4155e
briantimothykarani <timothykaranibrian@gmail.com>
2026-02-04 14:01:16 +0300

    Fix routing and prepare GitHub Pages deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import BrokersPage from "./pages/BrokersPage";
 import Landingpage from "./pages/Landingpage";
 import ComparePage from "./pages/ComparePage";
-import BrokerCard from "./components/BrokerCard"; // ✅ Import BrokerCard
+import BrokerCard from "./components/BrokerCard";
 
-const App = () => (
-  <BrowserRouter>
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Landingpage />} />
-        <Route path="/brokers" element={<BrokersPage />} />
-        <Route path="/brokers/comparepage" element={<ComparePage />} />
-        <Route path="/brokers/:id" element={<BrokerCard />} />{" "}
-        {/* ✅ Dynamic route for BrokerCard */}
-      </Routes>
-    </Layout>
-  </BrowserRouter>
-);
+const App = () => {
+  return (
+    <HashRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Landingpage />} />
+          <Route path="/brokers" element={<BrokersPage />} />
+          <Route path="/brokers/comparepage" element={<ComparePage />} />
+          <Route path="/brokers/:id" element={<BrokerCard />} />
+        </Routes>
+      </Layout>
+    </HashRouter>
+  );
+};
 
 export default App;
